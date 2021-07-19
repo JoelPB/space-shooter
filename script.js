66f72c6c21c48731dc508b7531fe1fd663e09c85
JoelPB <coffee.program.joel@gmail.com>
2021-07-19 16:26:01 -0300

    até a parte 3

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-area');
 
 // Moviemnto e tiro da nave
-function flyAhip(event) {
-    if(event.key === 'ArrayUp') {
+function flyShip(event) {
+    if(event.key === 'ArrowUp') {
         event.preventDefault();
         moveUp();
-    } else if(event.key === 'ArrayDown') {
+    } else if(event.key === 'ArrowDown') {
         event.preventDefault();
         moveDown();
     } else if(event.key === ' ') {
@@ -27,3 +27,17 @@ function moveUp() {
         yourShip.style.top = `${position}px`;
     }
 }
+
+function moveDown(){
+    let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+
+    if (topPosition === '550px') {
+        return;
+    } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+    }
+}
+
+window.addEventListener('keydown', flyShip);
